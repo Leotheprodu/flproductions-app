159bb4a6528afe1019a206568ff37239628829cd
Leo Serrano <leovpc@gmail.com>
2022-10-21 04:19:02 -0600

    lista la pagina de error

--- build/js/pages/error-page.jsx
+++ build/js/pages/error-page.jsx
@@ -4,29 +4,38 @@ import { useRouteError } from "react-router-dom";
 
 
 export default function ErrorPage() {
-
+  
+  
   const error = useRouteError();
   console.error(error);
- /*  useEffect(() => { document.body.style.backgroundColor = '#1ab5e6' }, []) */
+  useEffect(() => { document.body.style.backgroundColor = '#1ab5e6' }, [])
   return (
     <div className='error-page contenedor'>
       <Helmet>
         <title>FLProductions | Error</title>
       </Helmet>
+      <div className='error-page__errorType-padre'>
+      <p className='error-page__errorType'>
+          <i>{error.status || error.message}</i>
+        </p>
+      </div>
       <h1>¡Oops!</h1>
-      <h2>Parece que la Página no existe</h2>
+      
+      <h2>Parece que este enlace no existe</h2>
       <div className='error-page__redirigir'>
 
-        <p className=''>En <span>10</span> segundos te llevaremos a la página de Inicio</p>
-        <p>o</p>
+        <p className='error-page__redirigir__text'>Te recomendamos ir a la página principal</p>
+        <a href="/">
+          <div className='error-page__redirigir__button'>
+            Ir
+          </div>
+        </a>
         
-        <button>Click Aqui y Contáctanos</button>
-
 
       </div>
-      <p>
-        <i>{error.status || error.message}</i>
-      </p>
+      
+      
     </div>
   );
+  
 }
