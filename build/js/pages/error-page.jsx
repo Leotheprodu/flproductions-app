1e427b609306c44fec0f3dbfd630ee935972fe81
Leo Serrano <leovpc@gmail.com>
2022-10-21 01:28:24 -0600

    creando errorpage

--- build/js/pages/error-page.jsx
+++ build/js/pages/error-page.jsx
@@ -1,20 +1,31 @@
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { useRouteError } from "react-router-dom";
 
+
 export default function ErrorPage() {
-  
+
   const error = useRouteError();
   console.error(error);
-
+ /*  useEffect(() => { document.body.style.backgroundColor = '#1ab5e6' }, []) */
   return (
-    <div id="error-page">
+    <div className='error-page contenedor'>
       <Helmet>
         <title>FLProductions | Error</title>
       </Helmet>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <h1>¡Oops!</h1>
+      <h2>Parece que la Página no existe</h2>
+      <div className='error-page__redirigir'>
+
+        <p className=''>En <span>10</span> segundos te llevaremos a la página de Inicio</p>
+        <p>o</p>
+        
+        <button>Click Aqui y Contáctanos</button>
+
+
+      </div>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error.status || error.message}</i>
       </p>
     </div>
   );
