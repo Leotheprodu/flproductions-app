5d39f3f9742b69788aa86539a7c0265478d62904
Leo Serrano <leovpc@gmail.com>
2022-11-05 00:38:08 -0600

    backendworking

---
+++ app.mjs
@@ -0,0 +1,30 @@
+
+import path from "path";
+import express from "express";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const app = express();
+const PUERTO = process.env.PORT || 5000;
+
+// Hacer que node sirva los archivos de nuestro app React
+app.use(express.static(path.resolve(__dirname, './dist')));
+
+// Manejar las peticiones GET en la ruta /api
+app.get("/api", (req, res) => {
+  res.json({ message: "FLProductions" });
+});
+
+// Todas las peticiones GET que no hayamos manejado en las líneas anteriores retornaran nuestro app React
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
+});
+
+
+app.listen(PUERTO, () => {
+console.log(`El servidor esta escuchando en el puerto ${PUERTO}...`);
+
+});
