8b577a10ffb403eb6e9fbf626039f65d26a1ac62
Leo Serrano <leovpc@gmail.com>
2022-11-22 00:33:57 -0600

    conexion a bd

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const path = require("path");
 const express = require("express");
-
+const cors = require("cors");
+const mysql = require("mysql2");
 
 const app = express();
+app.use(cors());
 const PUERTO = process.env.PORT || 5000;
+
+const credentials={
+  host:'localhost',
+  user: 'root',
+  password:'Lsound2022',
+  database: 'flproductions'
+}
 
 // Hacer que node sirva los archivos de nuestro app React
 app.use(express.static(path.resolve(__dirname, './dist')));
 
-app.use((req, res, next) => {
+//si no tengo cors, debo usar esto
+/* app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-});
+}); */
 
 
 // Manejar las peticiones GET en la ruta /api
@@ -23,10 +33,54 @@ app.get("/api", (req, res) => {
   
 });
 
+app.get("/api/servicios", (req, res) => {
+  const connection = mysql.createConnection(credentials);
+  connection.query('SELECT * FROM servicios', (error, result) =>{
+    if(error) {
+      res.status(500).send(error);
+    }else {
+      res.status(200).send(result);
+    }
+  });
+});
+app.get("/api/servicios/precios", (req, res) => {
+  const connection = mysql.createConnection(credentials);
+  connection.query('SELECT * FROM servicios_precios', (error, result) =>{
+    if(error) {
+      res.status(500).send(error);
+    }else {
+      res.status(200).send(result);
+    }
+  });
+});
+app.get("/api/servicios/limitaciones", (req, res) => {
+  const connection = mysql.createConnection(credentials);
+  connection.query('SELECT * FROM servicios_limitaciones', (error, result) =>{
+    if(error) {
+      res.status(500).send(error);
+    }else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+/* app.get("/api/segundos", (req, res) =>{
+  let segundos = 0
+  setInterval(sumasegundos,1000)
+
+  function sumasegundos(){
+    segundos = segundos + 1
+  }
+  console.log(segundos);
+  
+  res.json({ segundos: `${segundos}` });
+}) */
+
 // Todas las peticiones GET que no hayamos manejado en las líneas anteriores retornaran nuestro app React
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
 });
+
 
 
 app.listen(PUERTO, () => {
