1382f9a5f2ed111b7b8da801a7e6ebba29855499
Leo Serrano <leovpc@gmail.com>
2023-01-24 03:45:51 -0600

    algunos cambios

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -3,6 +3,5 @@ export * from './contacto';
 export * from './inicio';
 export * from './nosotros';
 export * from './root';
-export * from './root2';
 export * from './servicios';
 export * from './error-page';
