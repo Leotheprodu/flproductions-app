1b714dd7424890f253450a65f3098b55ea6c0a1b
LeotheProdu <leovpc@gmail.com>
2023-03-09 14:32:06 -0600

    he agregado el codigo del formulario de registro

--- src/js/components/usuarios/SignUp.jsx
+++ src/js/components/usuarios/SignUp.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
+
+
 export const SignUp = () => {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes enviar los datos del formulario a tu servidor
+  };
+
   return (
-    <div className="SignUp">Register</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nombre:</label>
+        <input
+          type="text"
+          id="username"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Correo:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Contraseña:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Registrar</button>
+    </form>
+  );
+
 }
