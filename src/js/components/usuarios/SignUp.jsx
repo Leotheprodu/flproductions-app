9a79f49fd0b67997762b075aecb6638f2c5cced3
Nikky <nikolgomez05@gmail.com>
2023-03-09 21:21:31 -0600

    listo ya esta funcionando el componente signin

--- src/js/components/usuarios/SignUp.jsx
+++ src/js/components/usuarios/SignUp.jsx
@@ -1,15 +1,45 @@
 import { useState } from "react";
-
+import { useEnvLink } from "../hooks/UseEnvLink";
 
 
 export const SignUp = () => {
-  const [userName, setUserName] = useState('');
+  const [username, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const fecha_creacion = new Date().toISOString().slice(0, 10);
+  const role_id = 1;
+  const [envLink] = useEnvLink(process.env.NODE_ENV);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Aquí puedes enviar los datos del formulario a tu servidor
+    fetch(`${envLink}api/signup`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+
+      },
+      body: JSON.stringify({ email, password, username, fecha_creacion, role_id }),
+    })
+      .then(response => {
+        if (response.ok) {
+          // La solicitud se ha completado correctamente
+          // Redireccionar a la página de inicio
+          response = new Response(null, { status: 302, headers: { "Location": "/" } });
+        }
+        return response;
+      })
+      .then(response => {
+        setEmail('');
+        setPassword('');
+        setUserName('');
+      })
+      .catch(error => {
+        // Manejar el error aquí
+        console.log(error);
+      });
+
   };
 
   return (
@@ -19,7 +49,7 @@ export const SignUp = () => {
         <input
           type="text"
           id="username"
-          value={userName}
+          value={username}
           onChange={(e) => setUserName(e.target.value)}
         />
       </div>
