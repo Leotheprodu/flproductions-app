c59dfeb3b5f6c96b01782a000b5981fcef94018f
LeotheProdu <leovpc@gmail.com>
2023-06-24 03:47:08 -0600

    cambios esteticos, mejoras en colores, contrastes etc

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="es">
                 <Head>
                     <link rel="shortcut icon" href="/favicon.ico" />
                 </Head>
