c176065d5907b1207822ed766e05e7992c1b5177
Leonardo <leovpc@gmail.com>
2023-12-24 11:26:28 -0600

    Add WhatsApp icon and update styling in MessageComponent

--- components/layout/MessageComponent.tsx
+++ components/layout/MessageComponent.tsx
@@ -1,3 +1,4 @@
+import { IconBrandWhatsapp } from '@tabler/icons-react';
 import { UserAvatar } from '../users/UserAvatar';
 
 export const MessageComponent = () => {
@@ -6,13 +7,14 @@ export const MessageComponent = () => {
             <a
                 target="_blank"
                 href="https://wa.me/50663017707?text=Me%20gustar%C3%ADa%20saber%20m%C3%A1s%20de%20ustedes,%20"
-                className="MessageComponentIcon-content flex flex-col items-center justify-center"
+                className="cursor-pointer flex flex-col items-center justify-center gap-1"
             >
-                <div className="bg-blanco p-2 rounded-full">
-                    <UserAvatar user_id={44} size={5} />
+                <div className="rounded-full flex justify-center items-center m-0">
+                    <UserAvatar user_id={44} size={4} />
                 </div>
-                <span className="text-primario uppercase font-semibold bg-blanco rounded-xl p-2 mt-[-.5rem]">
-                    Chatea con Leo
+                <span className="text-success-400 text-center flex gap-1 items-center absolute bottom-[-1rem] bg-gris rounded-full px-1 shadow-sm">
+                    <IconBrandWhatsapp size={20} />
+                    <span className="text-sm">Whatsapp</span>
                 </span>
             </a>
         </div>
